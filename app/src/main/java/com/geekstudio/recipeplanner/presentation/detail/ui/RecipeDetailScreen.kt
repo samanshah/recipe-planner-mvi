63bb7ec235b473aec9cb238b243b942fce0984cd
samanshah <samanshahsavari3@gmail.com>
2026-06-09 14:57:43 +0330

    feat(detail): add recipe detail screen

--- app/src/main/java/com/geekstudio/recipeplanner/presentation/detail/ui/RecipeDetailScreen.kt
+++ app/src/main/java/com/geekstudio/recipeplanner/presentation/detail/ui/RecipeDetailScreen.kt
@@ -1,24 +1,80 @@
 package com.geekstudio.recipeplanner.presentation.detail.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import com.geekstudio.recipeplanner.core.ui.components.EmptyState
+import com.geekstudio.recipeplanner.presentation.detail.viewmodel.DetailViewModel
 
 @Composable
 fun RecipeDetailScreen(
-    recipeId: String
+    viewModel: DetailViewModel = hiltViewModel()
 ) {
 
-    Box(
-        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
-    ) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
 
-        Text(
-            text = "Recipe Detail\n$recipeId"
-        )
+    when {
+
+        state.isLoading -> {
+
+            CircularProgressIndicator()
+
+        }
+
+        state.recipe == null -> {
+
+            EmptyState(
+                message = "Recipe not found"
+            )
+
+        }
+
+        else -> {
+
+            val recipe = state.recipe!!
+
+            LazyColumn {
+
+                item {
+
+                    AsyncImage(
+                        model = recipe.imageUrl, contentDescription = null
+                    )
+
+                }
+
+                item {
+
+                    Text(
+                        text = recipe.title
+                    )
+
+                }
+
+                item {
+
+                    Text(
+                        text = recipe.category
+                    )
+
+                }
+
+                item {
+
+                    Text(
+                        text = recipe.instructions
+                    )
+
+                }
+
+            }
+
+        }
 
     }
 
