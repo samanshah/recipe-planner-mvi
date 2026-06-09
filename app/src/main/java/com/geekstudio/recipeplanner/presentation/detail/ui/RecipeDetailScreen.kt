1c89588aa197fd249779317e20f79ca6ad2b7b1d
samanshah <samanshahsavari3@gmail.com>
2026-06-09 14:59:26 +0330

    feat(detail): add favorite action

--- app/src/main/java/com/geekstudio/recipeplanner/presentation/detail/ui/RecipeDetailScreen.kt
+++ app/src/main/java/com/geekstudio/recipeplanner/presentation/detail/ui/RecipeDetailScreen.kt
@@ -1,7 +1,12 @@
 package com.geekstudio.recipeplanner.presentation.detail.ui
 
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -9,6 +14,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.geekstudio.recipeplanner.core.ui.components.EmptyState
+import com.geekstudio.recipeplanner.presentation.detail.contract.DetailIntent
 import com.geekstudio.recipeplanner.presentation.detail.viewmodel.DetailViewModel
 
 @Composable
@@ -37,6 +43,27 @@ fun RecipeDetailScreen(
         else -> {
 
             val recipe = state.recipe!!
+
+            IconButton(
+                onClick = {
+
+                    viewModel.onIntent(
+                        DetailIntent.ToggleFavorite
+                    )
+
+                }
+            ) {
+
+                Icon(
+                    imageVector =
+                        if (recipe.isFavorite)
+                            Icons.Default.Favorite
+                        else
+                            Icons.Default.FavoriteBorder,
+                    contentDescription = null
+                )
+
+            }
 
             LazyColumn {
 
